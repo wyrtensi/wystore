2d6414832c187b3b474709da42442516e7afc8bb
wyrtensi <189587229+wyrtensi@users.noreply.github.com>
2026-09-07 23:16:06 +0500

    Wy Store 0.1.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "dev.wystore"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.1.17"
+        versionCode = 19
+        versionName = "0.1.18"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
